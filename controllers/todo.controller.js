095565a11b069b6df6bc03a1859021e998933614
zli289 <zli289@gwu.edu>
2020-08-17 16:05:35 -0700

    init

---
+++ controllers/todo.controller.js
@@ -0,0 +1,54 @@
+const TodoModel = require('../model/todo.model');
+
+exports.createTodo = async (req, res, next) => {
+    try {
+        const createModel = await TodoModel.create(req.body);
+        res.status(201).json(createModel);
+    } catch (err)
+    {
+        next(err);
+    }
+} 
+
+exports.getTodos = async (req, res, next) => {
+  try {
+    const allTodos = await TodoModel.find({});
+    res.status(200).json(allTodos);
+  }
+  catch(err)
+  {
+      next(err);
+  }
+};
+
+exports.getTodoById = async (req, res, next) => {
+   try{
+    const todoModel = await TodoModel.findById(req.params.todoId);
+    if(todoModel){
+        res.status(200).json(todoModel);
+    }
+    else{
+        res.status(404).send();
+    }
+   }
+   catch(err)
+   {
+       next(err);
+   }
+};
+
+exports.deleteById = async (req, res, next) => {
+   try{
+    const todoModel = await TodoModel.deleteById(req.params.todoId);
+    if(todoModel){
+        res.status(200).json(todoModel);
+    }
+    else{
+        res.status(404).send();
+    }
+   }
+   catch(err)
+   {
+       next(err);
+   }
+};
---
+++ tests/integration/todo.controller.int.test.js
@@ -0,0 +1,55 @@
+const { response } = require('express');
+const request = require('supertest');
+const app = require('../../app');
+const newTodo = require('../mock-data/new-todo.json');
+
+const endpointUrl = '/todos/';
+
+let firstTodo;
+
+describe(endpointUrl, () => {
+	// it('DELETE by Id' + endpointUrl + ':todoId', async () => {
+	// 	const response = await (await request(app).delete(endpointUrl + firstTodo._id)).send(newTodo);
+	// 	expect(response.statusCode).toBe(200);
+	// 	expect(response.body.title).toBe(firstTodo.title);
+	// 	expect(response.body.done).toBe(firstTodo.done);
+	// });
+
+	it('GET by Id' + endpointUrl + ':todoId', async () => {
+		const response = await request(app).get(endpointUrl + firstTodo._id);
+		expect(response.statusCode).toBe(200);
+		expect(response.body.title).toBe(firstTodo.title);
+		expect(response.body.done).toBe(firstTodo.done);
+	});
+
+	it("Get todo by id doesn't exist" + endpointUrl + ':todoId', async () => {
+		const response = await request(app).get(endpointUrl + '5f281c93b9308e43044c899d');
+		expect(response.statusCode).toBe(404);
+	});
+
+	it('GET' + endpointUrl, async () => {
+		const response = await request(app).get(endpointUrl);
+
+		expect(response.statusCode).toBe(200);
+		expect(Array.isArray(response.body)).toBeTruthy();
+		expect(response.body[0].title).toBeDefined();
+		expect(response.body[0].done).toBeDefined();
+		firstTodo = response.body[0];
+	});
+
+	it('POST' + endpointUrl, async () => {
+		const response = await request(app).post(endpointUrl).send(newTodo);
+
+		expect(response.statusCode).toBe(201);
+		expect(response.body.title).toBe(newTodo.title);
+		expect(response.body.done).toBe(newTodo.done);
+	});
+	it('should return error 500 on malformed data with POST' + endpointUrl, async () => {
+		const response = await request(app).post(endpointUrl).send({ title: 'Missing done property' });
+
+		expect(response.statusCode).toBe(500);
+		expect(response.body).toStrictEqual({
+			message: 'Todo validation failed: done: Path `done` is required.'
+		});
+	});
+});
